aa71a1c098184914791f83112d4777b324991769
Laughing Jacky <4264332#qq.com>
2023-03-26 10:08:31 +0800

    feat: 增加备案号

--- src/lib/contentful.ts
+++ src/lib/contentful.ts
@@ -34,6 +34,8 @@ export interface BlogPost {
 
 export const contentfulClient = contentful.createClient({
   space: import.meta.env.CONTENTFUL_SPACE_ID,
-  accessToken: import.meta.env.DEV ? import.meta.env.CONTENTFUL_PREVIEW_TOKEN : import.meta.env.CONTENTFUL_DELIVERY_TOKEN,
-  host: import.meta.env.DEV ? 'preview.contentful.com' : 'cdn.contentful.com'
+  accessToken: import.meta.env.CONTENTFUL_DELIVERY_TOKEN,
+  host: 'cdn.contentful.com'
+  // accessToken: import.meta.env.DEV ? import.meta.env.CONTENTFUL_PREVIEW_TOKEN : import.meta.env.CONTENTFUL_DELIVERY_TOKEN,
+  // host: import.meta.env.DEV ? 'preview.contentful.com' : 'cdn.contentful.com'
 })
